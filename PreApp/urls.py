169c72c1d09abeedca5e9e25ca9cbd858190b2c5
Federico Ojeda <ojedaf45@gmail.com>
2023-02-11 10:17:58 -0300

    Actualización 2.1

--- PreApp/urls.py
+++ PreApp/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 
 urlpatterns = [
-    path('', inicio, name ='inicio'), 
+    path('', inicio, name ='inicio'),
+    path('/op_clientes', op_clientes, name ='op_clientes'),
+    path('/op_productos', op_productos, name ='op_productos'),
+    path('/op_forma_pago', op_forma_pago, name ='op_forma_pago'),
 ]
--- PreApp/views.py
+++ PreApp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request, 'PreApp/inicio.html')
+
+def op_clientes(request):
+    return render(request, 'PreApp/op_clientes.html')
+
+def op_forma_pago(request):
+    return render(request, 'PreApp/op_forma_pago.html')
+
+def op_productos(request):
+    return render(request, 'PreApp/op_productos.html')
